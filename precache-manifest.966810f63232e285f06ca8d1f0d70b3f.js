d8b054b300b91561a968c02e14e5fa4210b6e5dc
unknown <youmari300@gmail.com>
2020-06-15 14:58:00 +0100

    Updates

---
+++ precache-manifest.966810f63232e285f06ca8d1f0d70b3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd530416cd5019a06f57b3de47635b22",
+    "url": "/roboto/index.html"
+  },
+  {
+    "revision": "9e7d065f32a07fceac6b",
+    "url": "/roboto/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "aadc594b16c977f9cb2e",
+    "url": "/roboto/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "9e7d065f32a07fceac6b",
+    "url": "/roboto/static/js/2.49b6c3c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/roboto/static/js/2.49b6c3c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aadc594b16c977f9cb2e",
+    "url": "/roboto/static/js/main.686036b4.chunk.js"
+  },
+  {
+    "revision": "83d08b659b596dc14d93",
+    "url": "/roboto/static/js/runtime-main.ad920d17.js"
+  }
+]);
